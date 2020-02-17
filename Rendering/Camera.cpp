4c73d03c41650f41bf168c43924d7bbaff93d2e6
Tyler Bobik <shed412@gmail.com>
2020-02-16 20:37:51 -0500

    using switch

--- Rendering/Camera.cpp
+++ Rendering/Camera.cpp
@@ -50,14 +50,21 @@ void Camera::processMouseMovement(float xoffset, float yoffset){
 void Camera::processKeyboard(CameraMovement direction, float deltaTime) {
     // TODO: probably using the leran opengl implementation
     float velocity = movementSpeed * deltaTime;
-    if (direction == FORWARD)
-        position += lookDirection * velocity;
-    if (direction == BACKWARD)
-        position -= lookDirection * velocity;
-    if (direction == LEFT)
-        position -= rightVec * velocity;
-    if (direction == RIGHT)
-        position += rightVec * velocity;
+
+    switch(direction) {
+        case FORWARD:
+            position += lookDirection * velocity;
+        break;
+        case BACKWARD:
+            position -= lookDirection * velocity;
+        break;
+        case LEFT:
+            position -= rightVec * velocity;
+        break;
+        case RIGHT:
+            position += rightVec * velocity;
+        break;
+    }
     updateVectors();
 }
 
